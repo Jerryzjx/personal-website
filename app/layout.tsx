fb051986a52618efcabee26381c5e1381c4fb73e
JerryZhang0920 <jinxuan0920@gmail.com>
2023-09-12 19:26:08 -0400

    make changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
 	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
+		default: "Jerry Zhang",
+		template: "%s | Jerry Zhang",
 	},
 	description: "Software engineer at upstash.com and founder of planetfall.io",
 	openGraph: {
