7902b3254558d583a1bd7b01ebf88eccfcf477b0
JerryZhang0920 <jinxuan0920@gmail.com>
2023-09-12 19:17:46 -0400

    modified

--- app/page.tsx
+++ app/page.tsx
@@ -29,20 +29,19 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				Jerry Zhang
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hi, my name is Jerry, I'm a computer science student at the {" "}
 					<Link
 						target="_blank"
 						href="https://upstash.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Upstash
+						University of Waterloo
 					</Link>
 
 					<br />
@@ -52,7 +51,7 @@ export default function Home() {
 						href="https://unkey.dev"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						unkey.dev
+						my personal projects
 					</Link>{" "}
 					at night.
 				</h2>
