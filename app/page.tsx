d5e3c63a64fdfc89f91760278353ac62582ce823
JerryZhang0920 <jinxuan0920@gmail.com>
2023-09-14 18:27:41 -0400

    modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 						<Link
 							key={item.href}
 							href={item.href}
-							className="text-base duration-500 text-zinc-500 hover:text-zinc-300"
+							className="text-lg duration-500 text-zinc-500 hover:text-zinc-300"
 						>
 							{item.name}
 						</Link>
@@ -38,7 +38,7 @@ export default function Home() {
 			</div>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-base text-zinc-500 ">
+				<h2 className="text-lg text-zinc-500 ">
 					I'm a computer science student at the {" "}
 					<Link
 						target="_blank"
