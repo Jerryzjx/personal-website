65fed98c74aa1c6d1e4bff9b774c2f9f57f68746
JerryZhang0920 <jinxuan0920@gmail.com>
2023-09-16 11:42:52 -0400

    modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Particles from "./components/particles";
 
 
 const navigation = [
-	{ name: "Projects", href: "/contact" },
+	{ name: "Projects", href: "/projects" },
 	{ name: "Contact", href: "/contact" },
 ];
 
